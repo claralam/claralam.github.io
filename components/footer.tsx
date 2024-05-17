96e2b07572df8548964039db864d499c6e521b9f
Clara Lam <clara2k2@gmail.com>
2024-05-17 16:59:44 -0400

    Added footer to website

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="text-xs">
+        &copy; 2024 created from scratch by Clara Lam :)
+      </small>
+    </footer>
+  )
+}
