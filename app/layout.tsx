01415c052b57886567ec24ca8b3e855ce3985c1b
Clara Lam <clara2k2@gmail.com>
2024-05-17 16:29:52 -0400

    Changed navbar to disappear on scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
-        
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative py-28 sm:pt-36`}>
+
       <div className="bg-[#ecdefc] absolute bg-center top-[-1rem] -z-10 h-[31.25rem] w-[60rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 
       <Header />
--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import React from 'react';
-import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from 'next/link';
 
 export default function Header() {
+    const { scrollY } = useScroll();
+    const [hidden, setHidden] = useState(false);
+
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const previous = scrollY.getPrevious();
+        if (previous && latest > previous && latest > 150) {
+            setHidden(true);
+        } else {
+            setHidden(false);
+        }
+    })
+    
     return (
     <header className="z-[999] relative flex flex-col items-center px-4">
         <motion.nav 
             className="flex fixed w-full max-w-[48rem] top-[0.15rem] h-12 py-2 mx-auto justify-between sm:top-[1.7rem] sm:h-[initial] sm:py-0"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            variants={{
+                visible: { y: 0 },
+                hidden: { y: -100 },
+            }}
+            // initial={{ y: -100, opacity: 0 }}
+            // animate={{ y: 0, opacity: 1 }}
+            animate={hidden ? "hidden" : "visible"}
         >
-            <Link className="flex flex-wrap items-center font-medium text-gray-500 text-lg sm:w-[initial] sm:flex-nowrap hover:text-gray-950 transition" href="/">Clara Lam</Link>
+            <Link className="flex flex-wrap items-center font-medium text-gray-500 text-lg sm:w-[initial] sm:flex-nowrap hover:text-purple-950 transition" href="/">Clara Lam</Link>
             <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 text-lg sm:w-[initial] sm:flex-nowrap sm:gap-5">
                 {
                     links.map(link => (
@@ -22,7 +39,7 @@ export default function Header() {
                             key={link.hash}
                         >
                             <Link 
-                                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                                className="flex w-full items-center justify-center px-3 py-3 hover:text-purple-950 transition"
                                 href={link.hash}>{link.name}</Link>
                         </li>
                     ))
